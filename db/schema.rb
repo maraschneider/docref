995c1d20ce5629551e10b25a8247a9621cca432f
Maraike Schneider <maschn93+github@gmail.com>
2019-11-25 16:10:59 +0100

    changes DB schemes according to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_142358) do
+ActiveRecord::Schema.define(version: 2019_11_25_145820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "approval_fields", force: :cascade do |t|
+    t.bigint "approval_id"
+    t.bigint "field_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["approval_id"], name: "index_approval_fields_on_approval_id"
+    t.index ["field_id"], name: "index_approval_fields_on_field_id"
+  end
+
+  create_table "approvals", force: :cascade do |t|
+    t.string "headline"
+    t.text "content"
+    t.bigint "specialty_id"
+    t.bigint "giver_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["giver_id"], name: "index_approvals_on_giver_id"
+    t.index ["receiver_id"], name: "index_approvals_on_receiver_id"
+    t.index ["specialty_id"], name: "index_approvals_on_specialty_id"
+  end
+
+  create_table "clinics", force: :cascade do |t|
+    t.string "name"
+    t.string "consultation_times"
+    t.string "website"
+    t.string "phone_number"
+    t.string "email"
+    t.string "clinic_picture"
+    t.text "description"
+    t.string "address"
+    t.float "lat"
+    t.float "lng"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fields", force: :cascade do |t|
+    t.string "name"
+    t.bigint "specialty_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["specialty_id"], name: "index_fields_on_specialty_id"
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_fields", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "field_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["field_id"], name: "index_user_fields_on_field_id"
+    t.index ["user_id"], name: "index_user_fields_on_user_id"
+  end
+
+  create_table "user_specialties", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "specialty_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["specialty_id"], name: "index_user_specialties_on_specialty_id"
+    t.index ["user_id"], name: "index_user_specialties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +92,28 @@ ActiveRecord::Schema.define(version: 2019_11_25_142358) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "clinic_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "profile_picture"
+    t.string "title"
+    t.string "position"
+    t.text "bio"
+    t.boolean "only_private_insurance"
+    t.index ["clinic_id"], name: "index_users_on_clinic_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "approval_fields", "approvals"
+  add_foreign_key "approval_fields", "fields"
+  add_foreign_key "approvals", "specialties"
+  add_foreign_key "approvals", "users", column: "giver_id"
+  add_foreign_key "approvals", "users", column: "receiver_id"
+  add_foreign_key "fields", "specialties"
+  add_foreign_key "user_fields", "fields"
+  add_foreign_key "user_fields", "users"
+  add_foreign_key "user_specialties", "specialties"
+  add_foreign_key "user_specialties", "users"
+  add_foreign_key "users", "clinics"
 end
