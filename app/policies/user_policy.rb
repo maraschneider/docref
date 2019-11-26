e0843c4bab11b023ebdc3fa599fab63294a8f9ce
Sharina Alves <sharina.alves@mail.de>
2019-11-26 13:14:38 +0100

    add pundit and policies to all models but JTs

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,29 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def show?
+      true
+    end
+
+    def create?
+      true # maybe an if-statement later for our validation of the email?
+    end
+
+    def update?
+      is_user?
+    end
+
+    def delete?
+      false # what if a doctor wants to not be signed up anymore?
+    end
+  end
+
+  private
+
+  def is_user?
+    record.user == user
+  end
+end
