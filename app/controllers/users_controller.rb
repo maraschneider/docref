fcf0e9bd8b6ba54c2d1e112291fff45b5efa0fa3
Maraike Schneider <maschn93+github@gmail.com>
2019-11-26 15:29:21 +0100

    creates markers for clinics (TODO: access doctor)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   def index
 
     @doctors = policy_scope(User)
+
+    @clinics = Clinic.geocoded #returns flats with coordinates
+
+    @markers = @clinics.map do |clinic|
+      {
+        lat: clinic.latitude,
+        lng: clinic.longitude
+      }
+    end
   end
 
   def show
