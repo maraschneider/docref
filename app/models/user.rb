ada18cf4063c4b29b6d65fca7a739cdcb5a631f7
Sharina Alves <sharina.alves@mail.de>
2019-11-26 14:35:45 +0100

    add pgsearch and pg stuff in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  include PgSearch::module
+  pg_search_scope :search_by_specialty_and_field,
+    against: [:specialty, :field],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
