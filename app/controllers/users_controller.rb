ebbba281ad9a31d03e04b68f1c79daadc116f06b
Mike Yousef <msy@posteo.de>
2019-11-26 17:28:37 +0100

    basic framework for profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
-  def index
+  skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_doctor, only: [:show]
 
+  def index
     @doctors = policy_scope(User)
   end
 
   def show
+  end
 
+  private
+
+  def set_doctor
+    @doctor = User.find(params[:id])
+    authorize @doctor
   end
 end
