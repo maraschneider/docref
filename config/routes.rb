dd91a55ea05991072d75ad2fb1422641864a4848
Maraike Schneider <maschn93+github@gmail.com>
2019-11-26 14:19:44 +0100

    adds doctors_path for users#index
    and doctor_path for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doctors', to: 'users#index'
+  get 'doctors/:id', to: 'users#show', as: :doctor
 end
