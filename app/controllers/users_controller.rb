cac36867832b7274bb65a3120fba3d17ddf15c9b
Sharina Alves <sharina.alves@mail.de>
2019-11-27 10:31:12 +0100

    fixed search issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,18 @@ class UsersController < ApplicationController
       # @doctors = User.search_by_location(params[:location])
       @specialty = Specialty.search_by_specialty(params[:specialty_or_field])
       UserSpecialty.all.each do |user_spec|
-        next if user_spec.Specialty != @specialty
-        @doctors_array << User.find(user_spec)
+        next if user_spec.specialty != @specialty.first
+        @doctors_array << User.find(user_spec.user_id)
       end
 
       @field = Field.search_by_field(params[:specialty_or_field])
       UserField.all.each do |user_field|
-        next if user_field.id != @field
-        @doctors_array << User.find(user_field)
+        next if user_field.field != @field
+        @doctors_array << User.find(user_field.user_id)
       end
       return @doctors_array # instead of @doctors. Need to find a way to store the results there.
     else
-      @doctors = User.all
+      @doctors_array = User.all
     end
   end
 
