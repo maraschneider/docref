660b279327a7a938186eb9d283a05afa7a8cbb81
Sharina Alves <sharina.alves@mail.de>
2019-11-27 19:57:31 +0100

    add select2 and search dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,13 @@
 import "bootstrap";
+
+import 'select2/dist/css/select2.css';
+
+import { initSelect2 } from '../plugins/init_select2';
+
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+initSelect2();
