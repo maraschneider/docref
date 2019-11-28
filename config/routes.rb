a1633fbd6bf369465f240732eb9b0da1beb5b2a6
Maraike Schneider <maschn93+github@gmail.com>
2019-11-28 14:30:49 +0100

    refactor routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'doctors', to: 'users#index'
-  get 'doctors/:id', to: 'users#show', as: :doctor
-  get 'doctors/:doctor_id/approvals/new', to: 'approvals#new', as: :new_approval
+  resources :doctors, controller: :users, only: [:index, :show] do
+    resources :approvals, only: [:new, :create]
+  end
 
 end
