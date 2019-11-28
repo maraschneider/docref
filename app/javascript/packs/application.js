ba4729b46aafbc315c17ef92b926f3baf996b5da
Maraike Schneider <maschn93+github@gmail.com>
2019-11-28 20:54:35 +0100

    changes CSS class of checkbox-card on toggle
    ... dynamically using JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { setActiveClassToFieldChoice } from '../components/set_active_class_to_field_choice';
 
 initMapbox();
 initAutocomplete();
+setActiveClassToFieldChoice();
