ad68a41c49f32c92dddc9de4c125e819919b1876
Sharina Alves <sharina.alves@mail.de>
2019-12-01 16:28:52 +0100

    add more logic for tag color change when filter, but not activated

--- app/javascript/components/toggle_tags_on_approval_when_filter.js
+++ app/javascript/components/toggle_tags_on_approval_when_filter.js
@@ -1,8 +1,24 @@
 const toggleTagsOnApprovalWhenFilter = () => {
-  const tagChoices = document.querySelectorAll(".tag");
-  tagChoices.forEach((tag) => {
-    document.querySelectorAll('#tag-filter').addEventListener('change', (event) => {
+  //const url_string = window.location.href
+  //const url = new URL(url_string);
+  //const field_params = url.searchParams.get("approval_field%5B%5D");
+  //console.log(field_params);
+
+//  tags.forEach((tag) => {
+//    if (tag.innerText === field_params) {
+//       tag.classList.add('active');
+//    } else {
+//      tag.classList.remove('active');
+//    }
+//  });
+//};
+
+  const tags = document.querySelectorAll(".tag");
+  tags.forEach((tag) => {
+  const tagsChoice = document.getElementById('#tag-filter')
+    tagsChoice.addEventListener('change', (event) => {
       tag.classList.toggle("active");
+    });
   });
 };
 
