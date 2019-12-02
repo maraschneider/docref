614f68846df858a5a9a2484adeb3ec0f214953e4
Maraike Schneider <maschn93+github@gmail.com>
2019-12-02 21:28:48 +0100

    enables deletion of approval_fields on approval destroy ## To make a commit, type your commit message and press SUPER-ENTER. ## To cancel the commit, close the window. To sign off on the commit, ## press SUPER-S. ## ## You may also reference or close a GitHub issue with this commit. ## To do so, type `#` followed by the `tab` key.  You will be shown a ## list of issues related to the current repo.  You may also type ## `owner/repo#` plus the `tab` key to reference an issue in a ## different GitHub repo.
     app/models/approval.rb | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -2,7 +2,7 @@ class Approval < ApplicationRecord
   belongs_to :specialty
   belongs_to :giver, class_name: "User"
   belongs_to :receiver, class_name: "User"
-  has_many :approval_fields
+  has_many :approval_fields, dependent: :destroy
   has_many :fields, through: :approval_fields
 
   include PgSearch::Model
