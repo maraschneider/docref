b82e4c5fb38a84bf2eaebcd2e32a102fbd8588d2
Sharina Alves <sharina.alves@mail.de>
2019-12-02 17:56:19 +0100

    add placeholder to select2

--- app/javascript/plugins/init_select2_multi.js
+++ app/javascript/plugins/init_select2_multi.js
@@ -2,7 +2,9 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2Multi = () => {
-  $('.js-example-basic-multiple').select2();
+  $('.js-example-basic-multiple').select2({
+    placeholder: "Field"
+  });
 };
 
 export { initSelect2Multi };
--- app/javascript/plugins/init_select2_sing.js
+++ app/javascript/plugins/init_select2_sing.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2Sing = () => {
-  $('.js-example-basic-single').select2();
+  $('.js-example-basic-single').select2({
+    placeholder: "Specialty",
+    allowClear: true
+  });
 };
 
 export { initSelect2Sing };
